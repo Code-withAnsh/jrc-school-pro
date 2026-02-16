f6240468dd07d82e9d62e90d03020eeb571af559
Code-withAnsh <anshs9333@gmail.com>
2026-02-16 22:28:24 +0530

    fixed embedded git repository issue

---
+++ j.r.cschool/jrcschool/pdfs/LKG/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Express Server Running Successfully ✅");
+});
+
+app.listen(3000, () => {
+  console.log("Server Started on http://localhost:3000");
+});
